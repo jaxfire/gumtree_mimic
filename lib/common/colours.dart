f03344cd565cd9dd2637648459aeee89de34dea8
jamesashdepop <81640222+jamesashdepop@users.noreply.github.com>
2021-10-05 09:18:35 +0100

    Make colours constants.

--- lib/common/colours.dart
+++ lib/common/colours.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-Color lightGreen = Color(0xFF5BF200);
-Color darkGreen = Color(0xFF0E5B33);
-Color darkPink = Color(0xFF3C3141);
-Color lightPink = Color(0xFFE75461);
+const Color lightGreen = Color(0xFF5BF200);
+const Color darkGreen = Color(0xFF0E5B33);
+const Color darkPink = Color(0xFF3C3141);
+const Color lightPink = Color(0xFFE75461);
